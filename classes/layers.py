ab2a931a881732aab4978d407d8d779d3b37e271
smida taher <med.t.smida@gmail.com>
2025-10-27 04:50:15 +0100

    add the function to calcul the output of the furst layer

--- classes/layers.py
+++ classes/layers.py
@@ -2,7 +2,7 @@ import random
 
 class Layers:
     @staticmethod
-    def generateWidth(num):
+    def generateWeight(num):
         list = []
         for i in range(num):
             list.append(round(random.uniform(-1, 1), 1))
@@ -12,5 +12,5 @@ class Layers:
     def generateLayer(num,num2):
         list = []
         for i in range(num):
-            list.append(Layers.generateWidth(num2))
+            list.append(Layers.generateWeight(num2))
         return list 
--- test.py
+++ test.py
@@ -1,25 +1,22 @@
-import  pandas as pd 
 from classes.ProcessingData import ProcessingData as mpd
 from classes.layers import Layers as ly
-import random
 
-
-
+def calculOutput1( df , num , layer1 ):
+    list = df.drop(columns=['y']).iloc[num].tolist()
+    output = []
+    one = []
+    for j in range(16):
+        for i in range(48):
+            one.append(list[i] * layer1[j][i])
+        output.append(sum(one))
+        one.clear()
+    return output 
 
 df = mpd.getCsvData('bank.csv',';')
 df = mpd.hotCoding(df)
 
-layer1 = ly.generateLayer(16,50)
+layer1 = ly.generateLayer(16,48)
 layer2 = ly.generateLayer(2,16)
 
-print(layer1)
-print("\n-------------------------\n")
-print(layer2)
+print(len(calculOutput1(df,48,layer1)))
 
-
-
-
-
-# print(df.shape[1])
-# print(20*"---")
-# print(df)
